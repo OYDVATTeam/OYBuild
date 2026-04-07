71dbc4dc44db2125d552206359bb9a76a847ffe4
OYDVAT Team <bozoingbozo@gmail.com>
2026-04-07 18:12:17 +0200

    Create valacompile.hpp

---
+++ src/commands/valacompile.hpp
@@ -0,0 +1,10 @@
+#ifndef VALACOMPILE_HPP
+#define VALACOMPILE_HPP
+
+#include <string>
+#include <vector>
+
+// Pass the source file and the list of flags
+void handle_valacompile(const std::string& source, const std::vector<std::string>& flags);
+
+#endif
