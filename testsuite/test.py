d5853d23757ea2bdd827c8820f053d72d01970aa
OYDVAT Team <bozoingbozo@gmail.com>
2026-05-09 15:59:53 +0200

    Add test suite implementation in test.py
    Implement basic test suite functionality for OYBuild.

--- testsuite/test.py
+++ testsuite/test.py
@@ -1,5 +1,20 @@
+import os
+import sys
+
 print("OYBuild Test Suite")
-print("FATAL ERROR:")
-print("The test suite isn't implemented yet!")
-print("Solutions:")
-print("1. Wait until the test suite is implemented!")
+print("Testing OYBuild...")
+try:
+    os.chdir("test_empty_oybuildfile")
+except Exception as e:
+    print("ERROR: test_empty_oybuildfile")
+    print(e)
+    print("All tests failed. HINT: Please be in testsuite dir befor running test.py")
+    sys.exit(1)
+
+test1_result = os.system("../../oybuild")
+if test1_result == 0:
+    print("PASS: test_empty_oybuildfile")
+    sys.exit(0)
+else:
+    print("FAIL: test_empty_oybuildfile")
+    sys.exit(1)
